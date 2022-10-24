5bf93a9fb76ba95328fe258ec0cb3b375593dac4
Ethan-Prieur1 <ethan.prieur@mths.ca>
2022-10-24 13:48:31 +0000

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+/*
+ *
+ * The program calculates the amount of logs
+ *
+ * @author:  Ethan Prieur1
+ * @version: 1.0
+ * @since:   2022-10-24
+*/
+
+import java.util.Scanner;
+
+/**
+* This is the starting of main program.
+*/
+public abstract class Main {
+    /**
+     * Constant Variable Weight.
+     */
+    static final int LOGWEIGHT = 20;
+    /**
+     * Constant Variable Weight Limit.
+     */
+    static final int MAXTRUCKAMOUNT = 1100;
+
+    /**
+    * The starting main() function.
+    *
+    * @param args No args will be used.
+    */
+    public static void main(final String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter log length (0.25, 0.5 or 1.0): ");
+        float lengthNumber = sc.nextFloat();
+        float weight = lengthNumber * LOGWEIGHT;
+
+        weight = MAXTRUCKAMOUNT / weight;
+        System.out.println(+ weight);
+        System.out.println("logs can fit in the truck");
+        System.out.println("\nDone.");
+    }
+}
